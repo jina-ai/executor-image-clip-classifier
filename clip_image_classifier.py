48e442d875362594218ba14ffd38eac54aefc49a
Aziz Belaweid <azizbellaweid@hotmail.com>
2021-11-23 20:10:15 +0100

    fix: reformat code

--- clip_image_classifier.py
+++ clip_image_classifier.py
@@ -4,6 +4,7 @@ from typing import List, Optional
 import torch
 from jina.logging.logger import JinaLogger
 
+
 class CLIPImageClassifier(Executor):
     """Classifies an image given a list of text classes using CLIP model"""
 
@@ -16,7 +17,7 @@ class CLIPImageClassifier(Executor):
         traversal_paths: str = 'r',
         *args,
         **kwargs
-        ):
+    ):
         """
         :param classes: List of string that represents the classes an image can belong to
         e.g. ["this is a cat","this is a dog","this is a person"]
@@ -31,7 +32,7 @@ class CLIPImageClassifier(Executor):
         :param batch_size: Default batch size for encoding, used if the
             batch size is not passed as a parameter with the request.
         """
-        super().__init__(*args,**kwargs)
+        super().__init__(*args, **kwargs)
         self.logger = JinaLogger(getattr(self.metas, 'name', self.__class__.__name__))
         self.batch_size = batch_size
         self.traversal_paths = traversal_paths
@@ -41,40 +42,38 @@ class CLIPImageClassifier(Executor):
         self.preprocessor = CLIPProcessor.from_pretrained(
             self.pretrained_model_name_or_path
         )
-        self.model = CLIPModel.from_pretrained(
-            self.pretrained_model_name_or_path
-        )
+        self.model = CLIPModel.from_pretrained(self.pretrained_model_name_or_path)
         self.model.to(self.device).eval()
 
     @requests
     def classify(self, docs: DocumentArray, parameters: dict, **kwargs):
-        classes = parameters.get('classes',self.classes)
-        if classes == None :
+        classes = parameters.get('classes', self.classes)
+        if not classes:
             raise ValueError(
                 '``classes`` parameter is mandatory. Pass it in parameters'
                 'or in constructor. e.g. ["This is a cat","This is a person", "This is a dog"]'
             )
-        for docs_batch in docs.traverse_flat(parameters.get('traversal_paths', self.traversal_paths)).batch(
-            batch_size=parameters.get('batch_size', self.batch_size)
-        ):
-            image_batch = docs_batch.get_attributes('blob') 
+        for docs_batch in docs.traverse_flat(
+            parameters.get('traversal_paths', self.traversal_paths)
+        ).batch(batch_size=parameters.get('batch_size', self.batch_size)):
+            image_batch = docs_batch.get_attributes('blob')
             with torch.inference_mode():
-                input = self._generate_input_features(classes,image_batch)
+                input = self._generate_input_features(classes, image_batch)
                 outputs = self.model(**input)
-                logits_per_image = outputs.logits_per_image # this is the image-text similarity score
+                logits_per_image = (
+                    outputs.logits_per_image
+                )  # this is the image-text similarity score
                 probs = logits_per_image.softmax(dim=1)
-            for doc,prob in zip(docs_batch,probs):
+            for doc, prob in zip(docs_batch, probs):
                 prediction = max(prob)
-                doc.tags['class'] = {'label':classes[(prob==prediction).nonzero(as_tuple=True)[0]], 'score':float(prediction)}
+                doc.tags['class'] = {
+                    'label': classes[(prob == prediction).nonzero(as_tuple=True)[0]],
+                    'score': float(prediction),
+                }
 
-    def _generate_input_features(self,classes,images):
+    def _generate_input_features(self, classes, images):
         inputs = self.preprocessor(
-            text=classes,
-            images=images,
-            return_tensors='pt',
-            padding=True
-            )
-        inputs = {
-            k: v.to(torch.device(self.device)) for k, v in inputs.items()
-        }
+            text=classes, images=images, return_tensors='pt', padding=True
+        )
+        inputs = {k: v.to(torch.device(self.device)) for k, v in inputs.items()}
         return inputs
